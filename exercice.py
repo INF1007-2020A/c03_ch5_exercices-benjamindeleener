d61f0f7acf98a858f70ea0f2528adde3da0a7f52
Benjamin De Leener <benjamin.de-leener@polymtl.ca>
2020-09-21 21:21:54 +0200

    ex3

--- exercice.py
+++ exercice.py
@@ -24,7 +24,20 @@ def use_prefixes() -> List[str]:
 
 
 def prime_integer_summation() -> int:
-    return 0
+    nombrePremier = 0
+    nombre = 2
+    sommes = 0
+    while(nombrePremier < 100):
+        estPremier = True
+        for i in range(1, nombre+1):
+            if (nombre % i == 0 and i != 1 and i != nombre):
+                estPremier = False
+                break
+        if estPremier:
+            nombrePremier += 1
+            sommes += nombre
+        nombre += 1
+    return sommes
 
 
 def factorial(number: int) -> int:
