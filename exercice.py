873221442eac786d2a705db3e94551e30f955fe0
Benjamin De Leener <benjamin.de-leener@polymtl.ca>
2020-09-21 21:07:18 +0200

    Modified ex 1 and 2

--- exercice.py
+++ exercice.py
@@ -6,13 +6,21 @@ from typing import List
 
 
 def convert_to_absolute() -> float:
-    return 0.0
+    mon_nombre = float(input('Veuillez entrer un nombre: '))
+    if mon_nombre < 0:
+        mon_nombre = -mon_nombre
+    return mon_nombre
 
 
 def use_prefixes() -> List[str]:
     prefixes, suffixe = 'JKLMNOP', 'ack'
+    resultat = []
+    for prefixe in prefixes:
+        nom = prefixe + suffixe
+        resultat.append(nom)
+    return resultat
 
-    return [pre+suffixe for pre in prefixes]
+    #return [prefixe+suffixe for prefixe in prefixes]
 
 
 def prime_integer_summation() -> int:
@@ -28,17 +36,17 @@ def use_continue() -> None:
 
 
 def main() -> None:
-    print(f"La valeur absolue du nombre est {convert_to_absolute()}")
+    #print(f"La valeur absolue du nombre est {convert_to_absolute()}")
 
     print(f"La liste des noms générés avec les préfixes est: {use_prefixes()}")
 
-    print(f"La somme des nombres de 0 à 100 est: {prime_integer_summation()}")
+    #print(f"La somme des nombres de 0 à 100 est: {prime_integer_summation()}")
 
-    number = 10
-    print(f"La factiorelle du nombre {number} est: {factorial(number)}")
+    #number = 10
+    #print(f"La factiorelle du nombre {number} est: {factorial(number)}")
     
-    print(f"L'affichage de la boucle est:")
-    use_continue()
+    #print(f"L'affichage de la boucle est:")
+    #use_continue()
 
 
 if __name__ == '__main__':
